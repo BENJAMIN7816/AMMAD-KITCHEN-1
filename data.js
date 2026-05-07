742441c586c442555b30355639fef338e0785abe
BENJAMIN7816 <benjaminsebastian7816@gmail.com>
2026-05-07 12:24:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-8k-jp",
+      "name": "Panorama 8k JP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24796342044351682,
+          "pitch": 0.34606520549707476,
+          "rotation": 0,
+          "target": "2-panorama-8k-jp2"
+        },
+        {
+          "yaw": 1.4167565020636044,
+          "pitch": 0.13133848482820554,
+          "rotation": 0,
+          "target": "1-panorama-8k-jp1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama-8k-jp1",
+      "name": "Panorama 8k JP1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.42988012653623286,
+          "pitch": 0.023502200776334803,
+          "rotation": 0,
+          "target": "2-panorama-8k-jp2"
+        },
+        {
+          "yaw": -0.48178976209571545,
+          "pitch": 0.29181227950265587,
+          "rotation": 0,
+          "target": "0-panorama-8k-jp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-panorama-8k-jp2",
+      "name": "Panorama 8k JP2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.793579623445428,
+          "pitch": 0.3072585483780106,
+          "rotation": 0,
+          "target": "0-panorama-8k-jp"
+        },
+        {
+          "yaw": -0.6343395160369578,
+          "pitch": -0.09536828408515952,
+          "rotation": 0,
+          "target": "1-panorama-8k-jp1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
